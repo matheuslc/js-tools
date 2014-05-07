1615e6726c0c03273be21a43a9e084cb1af3c658
Matheus L. Carmo <me@matheuslc.com>
2014-05-07 17:47:48 -0300

    Add support to decimal number

--- randomize.js
+++ randomize.js
@@ -6,14 +6,15 @@
 */
 
 
-'use strict'
+
+
 
 /**
 * Select a random item in a range.
 *
-* @param	{Number}	firstvalue	First value of range.
-* @param	{Number}	secondvalue	Second value of range.
-* @returns	{Number}		A random number beetween first and second range.
+* @param    {Number}    firstvalue	First value of range.
+* @param    {Number}    secondvalue	Second value of range.
+* @returns  {Number}    A random number beetween first and second range.
 */
 
 
@@ -21,15 +22,30 @@ var randomize = function (firstvalue, secondvalue) {
                 var min = 0,
                     max = 0;
 
+                // Check min a max value
                 if (firstvalue > secondvalue) {
                     max = firstvalue;
                     min = secondvalue;
                 } else {
                     max = secondvalue;
                     min = firstvalue;
-                }
+                };
 
-                return Math.random()*(max-min+1)+min;
+                // Check if it's a decimal or integer number
+                if ((max % 1 === 0) && (min % 1 === 0)) {
+                    return Math.floor(Math.random()*(max-min+1)+min);
+                } else {
+                    return (Math.random() * (max-min)+min).toFixed(2);
+                };
 };
 
-console.log(randomize(10.3,10.5));
+
+/**
+ * Use Example
+ */
+
+// Integer Numbers
+var dice = randomize(1,6);
+
+// Decimal Numbers
+var variation = randomize(10.5,10.7);
