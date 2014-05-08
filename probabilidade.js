0c30b80c40c620e4e3651741fcec45555457afef
Matheus Lucca do Carmo <me@matheuslc.com>
2014-05-08 09:04:41 -0300

    Make intervalos function and add coments
    Function intervalos - Calculate classes number.

--- probabilidade.js
+++ probabilidade.js
@@ -1,81 +1,88 @@
 /** 
-* Name: Randomize 
+* Name: Histogramize 
 * Author: Matheus Lucca do Carmo (matheuslc)
 * @version: 0.1
-* @module randomize
+* @module histogramize
+*/
+
+/**
+ * Things gonna change. Be patience
 */
 
 'use strict'
 
-
-
 /**
-* Select a random item in a range.
-*
-* @param    {Number}    firstvalue	First value of range.
-* @param    {Number}    secondvalue	Second value of range.
-* @returns  {Number}    A random number beetween first and second range.
+ * Makes all counts to generate probability.
+ * @returns  {Array} array with final data
 */
 
-// Items
-
-
+// Item object
 var prob = {};
     
+    // Array with itens.
     prob.itens = [18,20,20,21,22,24,25,25,26,27,29,29,30,30,31,31,32,33,34,35,36,36,37,37,37,37,38,38,40,41,43,44,44,44,45,45,45,46,47,48,49,50,51,53,54,54,56,58,62,65];
     
+    /**
+     * Array size.
+     * @returns  {Number} array size.
+    */
     prob.n = function () {
         return this.itens.length;
     };
 
+    /**
+     * ROL - Sorts in ascending order array items.
+     * @returns  {Array} with the items sorted in ascending order. 
+    */
     prob.rol = function () {
         return this.itens.sort();
     };
 
+    /**
+     * Amplitude - Amplithe of items.
+     * @returns  {Number} amplitude of class. 
+    */
     prob.h = function () {
         return (Math.max.apply(Math, this.rol()) - Math.min.apply(Math, this.rol()));
     };
 
+    /**
+     * Log calc - Any calculation of log with different bases.
+	 * @param {Number} x Number of calculation.
+	 * @param {Number} y Log base.
+     * @returns  {Number} Log result. 
+    */
     prob.getBaseLog = function (x,y) {
         return Math.log(y) / Math.log(x);
     };
 
+    /**
+     * Classes number - Number of classes for the calculation.
+     * @returns  {Number} Classes number. 
+    */
     prob.numeroClasse = function () {
         return Math.ceil(1 + this.getBaseLog(2,this.n()));
     };
 
+    /**
+     * Interbal between classes.
+     * @returns  {Number} Distance classes value. 
+    */
     prob.tamanhoIntervalo = function () {
         return Math.ceil(this.h() / this.numeroClasse());
     };
 
+    /**
+     * Calculate classes number.
+     * @returns  {Array} with classes number. 
+    */
     prob.intervalos = function () {
-        var intervaloClasse = [],
+        var intervaloClasse = [min],
             min = Math.min.apply(Math, this.rol()),
-            rs = 0;
+            rs = min;
         for (var i = this.numeroClasse() - 1; i >= 0; i--) {
-            rs = min + 6 - 1;
-            console.log(rs);
-            intervaloClasse[i];
+            intervaloClasse[i]  = rs;
+            rs = rs + this.tamanhoIntervalo() - 1;
     };
+    	return intervaloClasse.reverse();
 };
-
-
-// Quantidade de itens
-// var n = items.length;
-
-// // Ordenando em ordem crescente
-// var rol = items.sort();
-
-// // Amplitude
-// var h = (Math.max.apply(Math, rol) - Math.min.apply(Math, rol));
-
-
-// // Número de classes
-// function getBaseLog(x, y) {
-//     return Math.log(y) / Math.log(x);
-// }
-// var k = Math.ceil(1 + getBaseLog(2,n));
-
-// // Intervalo de classes
-// var r = Math.floor(n / k);
-
